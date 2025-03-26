5cec672b858d6d85017923cbdb8ff2c4d1e3fa00
jeevan-aj <ambrahamjeevan@gmail.com>
2025-03-27 01:15:58 +0530

    Challenge 10 completed search target in rotated sorted array

---
+++ Challenge-10/searchIn-sortedArray.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left = 0, right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) return mid;
+
+        // Identify which half is sorted
+        if (nums[left] <= nums[mid]) { // Left half is sorted
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1; // Search in left half
+            } else {
+                left = mid + 1; // Search in right half
+            }
+        } else { // Right half is sorted
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1; // Search in right half
+            } else {
+                right = mid - 1; // Search in left half
+            }
+        }
+    }
+
+    return -1; // Target not found
+};
