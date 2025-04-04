ec5cdce2fe464be9699811be25d48d4ed2ecab9f
jeevan-aj <ambrahamjeevan@gmail.com>
2025-04-04 11:04:10 +0530

    Challenge 14 completed

---
+++ Challenge-14/counting_bits.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function(n) {
+    let result = []
+    for(let i = 0 ; i <= n ; i++){
+        result.push(i.toString(2).split('0').join('').length)
+    }
+    return result
+};
